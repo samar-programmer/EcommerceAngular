a78b84cab561d2bd90efb0926f52e51c1060b982
devabhi07 <abhisinghbhu55@gmail.com>
2022-01-31 17:41:28 +0530

    first

--- shopper/src/app/app.component.spec.ts
+++ shopper/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shopper'`, () => {
+  it(`should have as title 'angularshop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shopper');
+    expect(app.title).toEqual('angularshop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shopper app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularshop app is running!');
   });
 });
