9ac462ed2384f9696aa83e84aa877a9047256a58
samar-programmer <samarprogrammer7@gmail.com>
2022-02-02 04:02:45 -0800

    cart product

--- shopper/src/app/cart/cart.component.ts
+++ shopper/src/app/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../services/cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartDetails :any;
 
-  constructor() { }
+ totalproducts : any = 0;
+  constructor(private cartService :CartService) { }
 
   ngOnInit(): void {
+
+    let result : any = this.cartService.getAllProducts();
+
+    result
+    .subscribe({
+      next:(response:any)=>{
+       console.log(response);
+       this.cartDetails = response;
+       console.log(this.cartDetails);
+       this.totalproducts = this.cartDetails.length;
+      },
+      error:()=>{
+        alert("error");
+      }
+    })
+
   }
+
+increaseItemCount(item:any) {
+    item.productQuantity++;
+    item.quantityprice=item.productQuantity*item.productDiscountPrice;
+  };
+ decreaseItemCount = function(item:any) {
+   if(item.productQuantity>0){
+    item.productQuantity--;
+    item.quantityprice=item.productQuantity*item.productDiscountPrice;
+  
+   }
+ 
+  };
+  // $scope.sumCalc = function() {
+  //   var sum = 0;
+  //   angular.forEach($scope.itemList, function(item, index) {
+  //     sum += parseInt(item.quantity,10);
+  //   });
+  //   return sum;
+  // };
 
 }
---
+++ shopper/src/app/services/cart.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  baseUrl : string = 'http://localhost:10007/shopper/api/cart/';
+  constructor(private httpClient:HttpClient) { }
+
+  getAllProducts(){
+    let result:any = this.httpClient.get( this.baseUrl+"get-cart");
+    return result;
+  }
+
+}
